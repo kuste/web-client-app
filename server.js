07a2bd882cf0b522a1dbecb35c0544a18ed57d5e
ivica.kustura@rathmann.hr <kuuste@gmail.com>
2019-07-13 18:51:21 +0200

    c

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.static("./dist/web-app"))
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/web-app", "index.html"))
+  res.sendFile(path.join(__dirname, "web-app", "index.html"))
 })
 
 // Start the app by listening on the default Heroku port
