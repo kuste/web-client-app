1414732dd76499a831df85aaf872533b07541a1b
ivica.kustura@rathmann.hr <kuuste@gmail.com>
2019-07-13 18:48:04 +0200

    c

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express")
+const path = require("path")
 
-const app = express();
+const app = express()
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{web-app}}'));
+app.use(express.static("./dist/{{web-app}}"))
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/{{web-app}}/index.html'));
-});
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/{{web-app}}", "index.html"))
+})
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
